52b92f5a6660a561ba0b3504efd3664b0b4d3f07
Dan Adler <82471930+danbunkr@users.noreply.github.com>
2023-09-06 18:46:52 -0400

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,7 @@ import { WebPlugin } from '@capacitor/core';
 import type { PowerModePlugin } from './definitions';
 
 export class PowerModeWeb extends WebPlugin implements PowerModePlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async lowPowerModeEnabled(): Promise<{ lowPowerModeEnabled: boolean }> {
+    return { lowPowerModeEnabled: false };
   }
 }
